6bfc78d75d98c3209af8eee2da565df0b4a15f6f
tusharikaseth-cyber <tusharikaseth.2020@gmail.com>
2025-10-01 12:42:10 +0530

    Added shell script 590021621.sh for Day04by Tusharika seth

--- Day4/590021621.sh
+++ Day4/590021621.sh
@@ -1,11 +1,75 @@
-echo "Enter your name"
-read name
-a=4
-b=6
-c=$((a+10))
-echo $name
-echo "value of a and b are"
-echo $a
-echo $b
-echo "value of c is"
-echo $c
+#!/bin/bash
+
+# Print SAP ID
+echo "My sap id 590021621"
+
+# Print name
+echo "My name is Tusharika Seth from batch 1"
+
+# Show current directory
+pwd
+
+# List files in current directory
+ls
+
+# Create a new file and display its contents
+touch tush.txt
+cat tush.txt
+
+# Remove the file
+rm tush.txt
+
+# Create a new directory
+mkdir -p  tusha
+
+# List files in long format (correct syntax)
+ls -l
+
+# Change into the new directory
+cd tusha
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
