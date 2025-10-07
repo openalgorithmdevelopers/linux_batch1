4d1d04647d41a2beb76afbef95e20de8fcb41332
GursimratKaur23 <gursimratk673@gmail.com>
2025-10-07 13:20:09 +0530

    Sample file

---
+++ Day5/590022257.sh
@@ -0,0 +1,17 @@
+
+echo"enter two numbers"
+read num1
+read num2
+sum=$((num1+num2))
+echo"sum is:"
+echo $sum
+subtract=$((num1-num2))
+echo"subtraction is:"
+echo $subtract
+multiplication=$((num1*num2))
+echo"multiplication is:"
+echo $multiplication
+division=$((num1/num2))
+echo "division is:"
+echo $division
+
