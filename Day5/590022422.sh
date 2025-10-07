205acbbfc121e8408de961134589fffc7fca7086
isajal-11 <sajal1105@gmail.com>
2025-10-07 13:06:31 +0530

    adding my shell script file

---
+++ Day5/590022422.sh
@@ -0,0 +1,75 @@
+echo "Enter number 1"
+read num1
+echo "ENter number 2"
+read num2
+addition=$((num1+num2))
+echo "SUm is:"
+echo $addition
+subtraction=$((num1-num2))
+echo "SUb is:"
+echo $subtraction
+ 
+multiplication=$((num1*num2))
+echo "mul is:"
+echo $multiplication
+
+division=$((num1/num2))
+echo "division is:"
+echo $division
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
