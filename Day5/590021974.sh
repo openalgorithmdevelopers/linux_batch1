b2d7525876de3328cef631aacdc3343beedd3e17
Arpitmeh-glitch <mehrotraa245@gmail.com>
2025-10-07 13:11:42 +0530

    Arpit's commit

---
+++ Day5/590021974.sh
@@ -0,0 +1,15 @@
+echo "enter two numbers" 
+read num1 
+read num2
+c=$((num1+num2))
+sub=$((num1-num2))
+mul=$((num1*num2))
+div=$((num1/num2))
+echo "the sum is" 
+echo $c
+echo "the diffrecnce ia"
+echo $sub
+echo "the multiple is" 
+echo $mul
+echo "the div is"
+echo $div
