136f2777562f421417fa50e4b20b9c0e854ab4a3
kushgarg016 <kushgarg016@gmail.com>
2025-10-07 13:09:47 +0530

    add my shell script

---
+++ Day5/590021873.sh
@@ -0,0 +1,15 @@
+echo "Enter two numbers"
+read num1
+read num2
+add=$((num1+num2))
+echo "addition is:"
+echo $add
+sub=$((num1-num2))
+echo "Subtraction is:"
+echo $sub
+mul=$((num1*num2))
+echo "Multiplication is:"
+echo $mul
+div=$((num1/num2))
+echo "Division is"
+echo $div
