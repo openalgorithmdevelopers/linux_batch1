0ee0bcdb8dfbe4cea8fb5e868370ec866cba5b84
AARYANSHIMISHRA17 <am.aaryanshimishra17@gmail.com>
2025-10-08 05:13:56 +0000

    aaryanshi shell script q1

---
+++ Day5/590021677.sh
@@ -0,0 +1,15 @@
+echo "enter two number"
+read num1
+read num2
+sum=$((num1+num2))
+echo "sum is:"
+echo $sum
+dif=$((num1-num2))
+echo "dif is"
+echo $dif
+mul=$((num1*num2))
+echo "multiplication is:"
+echo $mul
+div=$((num1/num2))
+echo "division is;"
+echo $div
