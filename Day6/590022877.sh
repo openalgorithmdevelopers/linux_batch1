bdcd61b7c5ebec6b873f24f91d6806a0a39a9c1a
parasagarwal0305 <parasagarwal0305@gmail.com>
2025-10-08 11:28:19 +0530

    add

---
+++ Day6/590022877.sh
@@ -0,0 +1,8 @@
+read -p "Enter 1st digit= " d1
+read -p "Enter 2nd digit= " d2
+read -p "Enter 3rd digit= " d3
+read -p "Enter 4th digit= " d4
+read -p "Enter 5th digit= " d5
+sum=$(( d1 + d2 + d3 + d4 + d5 ))
+echo "sum= $sum"
+
