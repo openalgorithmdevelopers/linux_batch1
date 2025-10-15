6e0870020f5b327172ff4268a4f6a619aedc1c9b
ArpanDey2007 <arpan2007dey@gmail.com>
2025-10-15 11:33:08 +0530

    added

---
+++ Day7/590024562.sh
@@ -0,0 +1,33 @@
+# Start of while loop
+while true
+do
+    # Get number from user
+    read -p "Enter a number (0 to exit): " num
+
+    # Exit condition
+    if [ "$num" -eq 0 ]; then
+        echo "Exiting..."
+        break
+    fi
+
+    # 1) Print number of digits
+    digits=${#num}
+    echo "Number of digits: $digits"
+
+    # 2) Check odd or even
+    if (( num % 2 == 0 )); then
+        echo "The number is Even."
+    else
+        echo "The number is Odd."
+    fi
+
+    # 3) Check palindrome
+    rev=$(echo $num | rev)
+    if [ "$num" == "$rev" ]; then
+        echo "The number is a Palindrome."
+    else
+        echo "The number is not a Palindrome."
+    fi
+
+    echo "----------------------------"
+done
