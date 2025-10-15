28b318662a8f827476b742ab3847ddf20888085a
tusharikaseth-cyber <tusharikaseth.2020@gmail.com>
2025-10-15 11:36:06 +0530

    by tusharika

---
+++ Day8/590021621q1.sh
@@ -0,0 +1,33 @@
+read -p"enter a number " num
+revnum=$(echo "$num" | rev)
+if [[ "$num" == "$revnum" ]];
+
+
+then 
+
+
+    echo "$num is a pallindrome "
+
+
+else 
+
+
+    echo "$num is not a pallindrome"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+A
+A
+A
+fi
