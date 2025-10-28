71d743721ecda3532fb3947e270000c40879d11e
kushgarg016 <kushgarg016@gmail.com>
2025-10-28 12:45:34 +0530

    BY KUSH

---
+++ Day9/590021873.sh
@@ -0,0 +1,25 @@
+num=$1
+
+if [ $num ]
+then
+if [ $num -eq 0 ]
+then 
+   echo "Zero number"
+elif [ $num -gt 0 ]
+then
+echo "Positive number"
+else
+echo "Negative number"
+fi
+
+while [ $num -ne 0 ]
+do
+
+digcount=$((digcount+1))
+num=$((num/10))
+
+done
+echo "The given number has $digcount digits"
+else
+echo "No number given"
+fi
