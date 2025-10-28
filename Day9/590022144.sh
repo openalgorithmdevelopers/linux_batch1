798ff27e226fb385f0feedd73258000f6bbcd70e
aditya-rawat86 <rawatsunny747@gmail.com>
2025-10-28 18:46:13 +0530

    adding

--- Day9/590022144.sh
+++ Day9/590022144.sh
@@ -1,21 +1,7 @@
-echo "Number $1"
 num=$1
-if [ $num -gt 0 ]; then
-	echo "is positive"
-elif [ $num -lt 0 ]; then
-	echo "is negative"
-else
-	echo "is zero"
-fi
-
-if [ $num -eq 0 ]
-then
-    count=1
-else
-    while [ $num -ne 0 ]
-    do
-        num=$((num / 10))
-        count=$((count + 1))
-    done
-fi
-echo "Digits = $count"
+while [ $num -ne 0 ]
+do
+	num=$((num / 10))
+	count=$((count + 1))
+done
+echo "$count"
