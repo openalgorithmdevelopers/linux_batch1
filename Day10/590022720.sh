e8b81e01828de15487dba35e5c98b5afd1137129
Niharika <niharikaty07@gmail.com>
2025-10-29 11:19:55 +0530

    Niharika

---
+++ Day10/590022720.sh
@@ -0,0 +1,65 @@
+fileName="RohanSharma.txt"
+count=0
+searchForChar=" "   
+
+while IFS= read -n1 character
+do
+    if [ "$character" = "$searchForChar" ]; then
+        ((count++))
+    fi
+done < "$fileName"
+
+echo "Number of spaces in '$fileName': $count"
+
+fileName="RohanSharma.txt"
+count=0
+
+
+while IFS= read -n1 character
+do
+    ((count++))
+done < "$fileName"
+
+echo "Total number of characters in '$fileName': $count"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
