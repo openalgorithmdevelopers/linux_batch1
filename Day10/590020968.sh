8a881894a83c23cf8c2888b20a50837219d6b4a6
Shivam Tyagi <shivam.tyagi23123@gmail.com>
2025-10-29 10:40:53 +0530

    Task of Day 10

--- Day10/590020968.sh
+++ Day10/590020968.sh
@@ -4,19 +4,26 @@ if [ $# -ne 1 ]; then
   echo "Usage: $0 filename"
   exit 1
 fi
-
 filename="$1"
-
 if [ ! -f "$filename" ]; then
   echo "File not found!"
   exit 1
 fi
-
-spaces=$(grep -o " " "$filename" | wc -l)
-is_count=$(grep -o -w "is" "$filename" | wc -l)
-char_count=$(wc -m < "$filename")
-
+contents=$(cat "$filename")
+space_only=$(echo "$contents" | tr -cd ' ')
+spaces=${#space_only}
+char_count=${#contents}
+count=0
+text=" $contents "
+pos=0
+while true; do
+  match=$(expr index "${text:pos}" " is ")
+  if [ "$match" -eq 0 ]; then
+    break
+  fi
+  count=$((count + 1))
+  pos=$((pos + match + 2))
+done
 echo "Number of spaces: $spaces"
-echo "Number of times 'is' present: $is_count"
+echo "Number of times 'is' present: $count"
 echo "Total number of characters: $char_count"
-
