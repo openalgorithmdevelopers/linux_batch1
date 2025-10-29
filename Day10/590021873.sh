1c3629f9ecc623b1c00369677bfc180c486c33bf
kushgarg016 <kushgarg016@gmail.com>
2025-10-29 11:21:31 +0530

    BY KUSH

---
+++ Day10/590021873.sh
@@ -0,0 +1,26 @@
+fileName="RohanSharma.txt"
+
+count_spaces=0
+count_is=0
+count_chars=0
+prev_char=""
+
+while IFS= read -r -n1 character
+do
+    ((count_chars++))
+
+    if [ "$character" = " " ]; then
+        ((count_spaces++))
+    fi
+
+    if [ "$prev_char" = "i" ] && [ "$character" = "s" ]; then
+        ((count_is++))
+    fi
+
+    prev_char="$character"
+done < "$fileName"
+
+echo "Total characters = $count_chars"
+echo "Number of spaces = $count_spaces"
+echo "Number of 'is' =  $count_is"
+~
