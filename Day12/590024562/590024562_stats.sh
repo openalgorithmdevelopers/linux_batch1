8e44c999307a966813f6eff317e56ff217d81986
ArpanDey2007 <arpan2007dey@gmail.com>
2025-11-11 14:13:42 +0530

    Day12 task

--- Day12/590024562/590024562_stats.sh
+++ Day12/590024562/590024562_stats.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
+# Check if a filename was given when running the script
 if [ -z "$1" ]; then
-  echo "Usage: $0 filename"
+  echo "Please provide a filename as an argument."
+  echo "Example: $0 yourfile.txt"
   exit 1
 fi
 
-file=$1
-output="590024562_output.txt"
+# Save the input filename to a variable
+input_file=$1
 
-words=$(tr '[:upper:]' '[:lower:]' < "$file" | tr -c '[:alnum:]' '[\n*]' | grep -v '^$')
+output_file="590024562_output.txt"
 
-declare -A word_counts
-while read -r word; do
-  ((word_counts[$word]++))
+words=$(tr '[:upper:]' '[:lower:]' < "$input_file" | tr -c '[:alnum:]' '\n' | grep -v '^$')
+
+declare -A word_count
+
+while read word; do
+  ((word_count[$word]++))
 done <<< "$words"
 
-total_unique=${#word_counts[@]}
+unique_words=${#word_count[@]}
 
-sorted_words=$(for w in "${!word_counts[@]}"; do echo "${word_counts[$w]} $w"; done | sort -rn)
+word_list=""
+for word in "${!word_count[@]}"; do
+  word_list+="${word_count[$word]} $word"$'\n'
+done
+
+sorted_words=$(echo "$word_list" | sort -nr)
 
 top_five=$(echo "$sorted_words" | head -5)
 
@@ -25,10 +35,11 @@ top_five=$(echo "$sorted_words" | head -5)
   echo "Top five most occurring words:"
   echo "$top_five" | awk '{print $2 ": " $1}'
   echo
-  echo "Total number of unique words: $total_unique"
+  echo "Total number of unique words: $unique_words"
   echo
-  echo "Listing of all unique words and their frequency:"
+  echo "All unique words with their frequencies:"
   echo "$sorted_words" | awk '{print $2 ": " $1}'
-} > "$output"
+} > "$output_file"
 
-echo "Statistics saved to $output"
+echo "Your word statistics have been saved to $output_file"
+
