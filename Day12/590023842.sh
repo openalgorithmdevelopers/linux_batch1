1aa909f58ce25c6d4f84bb7d1590fcd9d33ce83a
Raghav590023842 <raghavisnotsus@gmail.com>
2025-11-11 13:05:31 +0530

    Raghav Vij

--- Day12/590023842.sh
+++ Day12/590023842.sh
@@ -5,7 +5,7 @@ then
 fi
 
 file=$1
-output="file_name_stats.txt"
+output="${file}_stats.txt"
 
 echo "Top 5 most occurring words:" > $output
 cat $file | tr " " "\n" | sort | uniq -c | sort -nr | head -5 >> $output
