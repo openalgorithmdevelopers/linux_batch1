13138b77a2d3862b1d455eb821489bb6ff86c54e
isajal-11 <sajal1105@gmail.com>
2025-11-18 13:13:52 +0530

    Adding my shell script identifying unique characters

---
+++ Day12/590022422.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+file="$1"
+
+read_words() {
+    words=($(tr -c 'a-zA-Z' '\n' < "$file" | tr 'A-Z' 'a-z'))
+}
+
+count_unique_words() {
+    unique=()
+    freq=()
+
+    for w in "${words[@]}"; do
+        [[ -z "$w" ]] && continue
+        found=0
+
+        for i in "${!unique[@]}"; do
+            if [[ "${unique[$i]}" == "$w" ]]; then
+                freq[$i]=$((freq[$i] + 1))
+                found=1
+                break
+            fi
+        done
+
+        if [[ $found -eq 0 ]]; then
+            unique+=("$w")
+            freq+=(1)
+        fi
+    done
+}
+
+write_output() {
+    echo "Total number of unique words: ${#unique[@]}" > stats.txt
+    echo "" >> stats.txt
+    echo "Unique words and their frequencies:" >> stats.txt
+
+    for i in "${!unique[@]}"; do
+        echo "${unique[$i]} : ${freq[$i]}" >> stats.txt
+    done
+}
+
+read_words
+count_unique_words
+write_output
+
+echo "Output saved to stats.txt"
+
