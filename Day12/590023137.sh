ff2b77bc45ace0d06bb7ca5a6c2ac8b792209156
kaushalisallyouneed24 <mannukaushal24@gmail.com>
2025-11-18 13:24:27 +0530

    updated by manasvi

--- Day12/590023137.sh
+++ Day12/590023137.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
 if [ $# -ne 1 ]; then
-    echo "Usage: $0 <input_file>"
-    exit 1
+  echo "Usage: $0 <input_file>"
+  exit 1
 fi
 
-input_file="$1"
-output_file="file_name_stats.txt"
+input="$1"
+output="file_name_stats.txt"
+declare -A word_count
 
-tr '[:upper:]' '[:lower:]' < "$input_file" | tr -c 'a-z0-9\n' '\n' | grep -v '^$' | sort | uniq -c | sort -nr > temp_counts.txt
+while read -r line; do
+  for word in $line; do
+    word=$(echo "$word" | tr '[:upper:]' '[:lower:]' | tr -cd 'a-z0-9')
+    if [ -n "$word" ]; then
+      ((word_count["$word"]++))
+    fi
+  done
+done < "$input"
 
-echo "Top five most occurring words:" > "$output_file"
-head -n 5 temp_counts.txt | awk '{print $2": "$1}' >> "$output_file"
+echo "Top five most occurring words:" > "$output"
+for word in "${!word_count[@]}"; do
+  echo "${word_count[$word]} $word"
+done | sort -nr | head -5 >> "$output"
 
-unique_count=$(wc -l < temp_counts.txt)
+echo "" >> "$output"
+echo "Total number of unique words:" >> "$output"
+echo "${#word_count[@]}" >> "$output"
 
-echo "" >> "$output_file"
-echo "Total number of unique words:" >> "$output_file"
-echo "$unique_count" >> "$output_file"
-
-rm temp_counts.txt
-
-echo "Output written to file_name_stats.txt"
+echo "Done! Check $output"
 
