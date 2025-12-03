b4afa15b718ab2b364e372aeeb356a39e6b78857
Purvansh <purvanshgupta07@gmail.com>
2025-12-03 12:01:44 +0530

    Create 590022338.sh

---
+++ Day10/590022338.sh
@@ -0,0 +1,25 @@
+fileName="linux.txt"
+
+count_spaces=0
+count_is=0
+count_chars=0
+prev_char=""
+
+while IFS= read -r -n1 character
+do
+    ((count_chars++))
+
+    if [ "$character" = " " ]; then
+        ((count_spaces++))
+    fi
+
+    if [ "$prev_char" = "i" ] && [ "$character" = "s" ]; then
+        ((count_is++))
+    fi
+
+    prev_char="$character"
+done < "$fileName"
+
+echo "Total characters = $count_chars"
+echo "Number of spaces = $count_spaces"
+echo "Number of 'is' =  $count_is"
